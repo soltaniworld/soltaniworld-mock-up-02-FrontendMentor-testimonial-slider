0e7ec3b3b3dbc98281789d59a40e40769a191894
Arash Soltani <soltaniworld@gmail.com>
2020-09-03 15:07:12 -0400

    logic works, but quote bg is missing after adding absolute position to slides--content

--- main.js
+++ main.js
@@ -3,28 +3,73 @@ const btnPrev = document.querySelector(".media-nav__btn--left");
 
 const slides = document.querySelectorAll(".slides");
 
-let index = 1
+let slideIndex = 1
+let slideLength = findSlideLength(slides);
+let slideShowTime = 5000
+let playInterval
 
-// Hide all quotes
-slides.forEach((slide)=>{
-  if (slide.classList.contains('slide-2')){
-    console.log(slide);
-    slide.classList.add("ease-out")
-    setTimeout(()=>{slide.classList.add("hide")}, 500)
-    
-  }
-});
+showSlide(slideIndex);
+autoPlay();
 
 btnNext.addEventListener('click', ()=>{
-  changeSlide(index)
-  quote.classList.toggle("ease-out");
+  clearInterval(playInterval);
+  autoPlay();
+  changeSlideIndex(+1);
+  showSlide(slideIndex);
+});
+btnPrev.addEventListener('click', ()=>{
+  clearInterval(playInterval);
+  autoPlay();
+  changeSlideIndex(-1);
+  showSlide(slideIndex);
 });
 
-function changeSlide(i){
-  if (i==1){
-    index = 2;
+
+function changeSlideIndex(i){
+  slideIndex += i
+  if (slideIndex > slideLength ){
+    slideIndex = slideLength;
   }
-  else{
-    index = 1;
+  else if (slideIndex < 1){
+    slideIndex = 1;
   }
-};
+}
+function showSlide(i){
+  slides.forEach((slide)=>{
+    if (slide.classList.contains(`slide-${i}`)){
+      slide.classList.add("ease-in");
+      slide.classList.remove("ease-out"); 
+    }
+    else{
+      slide.classList.remove("ease-in");
+      slide.classList.add("ease-out");
+    }
+});
+}
+
+//find number of slides available
+function findSlideLength(slides){
+  let length = 0
+  slides.forEach((slide)=>{
+    slide.classList.forEach((e)=>{
+      if (e.startsWith('slide-')){
+        let i = Number(e.slice('slide-'.length))
+        if (i > length){
+          length = i;
+        }
+      }
+    })
+  })
+  return length;
+}
+
+// set interval to auto change slide
+function autoPlay(){
+  playInterval = setInterval(()=>{
+    showSlide(slideIndex);
+    slideIndex+=1;
+    if (slideIndex > slideLength){
+      slideIndex = 1;
+    }
+  }, slideShowTime);
+}
